d8651b760037254dfd46d07aebafb07c47a2f15a
Praveen Selvaraj <sc19ps@leeds.ac.uk>
2022-02-23 17:10:05 +0000

    object's initial position used as shared ID; ownership and position update for new peers

--- VRProject/Assets/Scripts/Block.cs
+++ VRProject/Assets/Scripts/Block.cs
@@ -4,49 +4,99 @@ using UnityEngine;
 using Ubiq.Messaging;
 using Ubiq.XR;
 using System;
+using Ubiq.Rooms;
 
 public class Block : MonoBehaviour, IGraspable, INetworkComponent, INetworkObject
 {
 
     private Hand grasped;
 
-    NetworkId INetworkObject.Id => new NetworkId(1001);
     private NetworkContext context;
+
+    // 16-digit hex
+    NetworkId INetworkObject.Id => new NetworkId("f8cdefa3a15f5e6d");
+
+    public NetworkId shared_id; 
+
+    private RoomClient client;
+
+    private bool owner = false; 
 
     struct Message
     {
+        public NetworkId who; 
         public Vector3 position;
         public Quaternion rotation;
+
+        public Message(Vector3 pos, Quaternion rot, NetworkId who)
+        {
+            this.who = who;
+            this.position = pos;
+            this.rotation = rot; 
+        }
     }
 
     void INetworkComponent.ProcessMessage(ReferenceCountedSceneGraphMessage message)
     {
         var msg = message.FromJson<Message>();
-        transform.localPosition = msg.position;
-        transform.rotation = msg.rotation;
+
+        // update position only if
+        // the message comes from an object of the same shared ID
+        if (msg.who == shared_id)
+        {
+            transform.localPosition = msg.position;
+            transform.rotation = msg.rotation;
+        } 
     }
 
     // Start is called before the first frame update
     void Start()
     {
         context = NetworkScene.Register(this);
+
+        client = context.scene.GetComponentInChildren<RoomClient>();
+
+        client.OnPeerAdded.AddListener(OnPeerAdded);
+
+        shared_id = new NetworkId((uint)(Math.Pow(10, 5)*(transform.localPosition.x + 
+                                        transform.localPosition.y + 
+                                        transform.localPosition.z) ));
+    }
+
+    private void OnPeerAdded(IPeer peer)
+    {
+        if (peer.UUID == client.Me.UUID)
+        {
+            return; 
+        } else
+        {
+            Message message;
+            message.position = transform.localPosition;
+            message.rotation = transform.rotation;
+            message.who = shared_id;
+
+            context.SendJson(message);
+        }
+
     }
 
     void IGraspable.Grasp(Hand controller)
     {
         grasped = controller;
+        owner = true; 
     }
 
     void IGraspable.Release(Hand controller)
     {
         grasped = null;
+        owner = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
         // If the block is held by a player
-        if (grasped)
+        if (grasped && owner)
         {
             // Match the position and orientation of the hand
             transform.localPosition = grasped.transform.position;
@@ -56,6 +106,8 @@ public class Block : MonoBehaviour, IGraspable, INetworkComponent, INetworkObjec
             Message message;
             message.position = transform.localPosition;
             message.rotation = transform.rotation;
+            message.who = shared_id; 
+
             context.SendJson(message);
         }
     }
