bc6f8189b1a43de9114554339d006d82ff6750a2
harveymannering <60523103+harveymannering@users.noreply.github.com>
2022-03-09 14:43:54 +0000

    Put everything in one scene

--- VRProject/Assets/Scripts/BluePrint.cs
+++ VRProject/Assets/Scripts/BluePrint.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class BluePrint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float RotationSpeed = 20.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
     }
 }
