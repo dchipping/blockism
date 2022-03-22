856afc604d962d504ac237078fed45863855a449
Praveen Selvaraj <sc19ps@leeds.ac.uk>
2022-03-22 13:17:50 +0000

    network scene prefab, bpdoor

--- VRProject/Assets/Scripts/BPRoomDoor.cs
+++ VRProject/Assets/Scripts/BPRoomDoor.cs
@@ -6,16 +6,14 @@ public class BPRoomDoor : MonoBehaviour
 {
     private List<string> colours;
     private List<int> colour_indexes;
-    public Material doorColour;
 
     RoleManager role_manager;
 
     // Start is called before the first frame update
-    void Start()
+    public void Init()
     {
-        doorColour = GameManager.blockColoursStatic[0];
         MeshRenderer mesh_rend = gameObject.GetComponent<MeshRenderer>();
-        mesh_rend.material = doorColour;
+        mesh_rend.material = GameManager.blockColoursStatic[0];
         role_manager = GameObject.Find("RoleManager").GetComponent<RoleManager>();
         colours = role_manager.GetAvatarRoles();
         colour_indexes = role_manager.GetAvatarColourIndexes();
@@ -29,19 +27,19 @@ public class BPRoomDoor : MonoBehaviour
 
     public void ChangeColour(int index)
     {
+        index = (index - 1) % GameManager.numOfPlayers;
         int col_idx = colour_indexes[index];
-        doorColour = GameManager.blockColoursStatic[col_idx];
         MeshRenderer mesh_rend = gameObject.GetComponent<MeshRenderer>();
-        mesh_rend.material = GameManager.blockColoursStatic[index];
+        mesh_rend.material = GameManager.blockColoursStatic[col_idx];
+
+       /* if (index > 0)
+        {
+            int prev_col_idx = colour_indexes[index - 1];
+            GameObject avatarNotAllowed = GameObject.FindGameObjectWithTag(colours[prev_col_idx]);
+            Physics.IgnoreCollision(gameObject.GetComponent<BoxCollider>(), avatarNotAllowed.gameObject.GetComponent<BoxCollider>(), false);
+        }*/
 
         GameObject avatarAllowed = GameObject.FindGameObjectWithTag(colours[col_idx]);
         Physics.IgnoreCollision(gameObject.GetComponent<BoxCollider>(), avatarAllowed.gameObject.GetComponent<BoxCollider>(), true);
-
-        if (index > 0)
-        {
-            int prev_col_idx = colour_indexes[index-1];
-            GameObject avatarNotAllowed = GameObject.FindGameObjectWithTag(colours[prev_col_idx]);
-            Physics.IgnoreCollision(gameObject.GetComponent<BoxCollider>(), avatarNotAllowed.gameObject.GetComponent<BoxCollider>(), false);
-        }
     }
 }
